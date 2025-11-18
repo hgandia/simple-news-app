04f3ead635b522f8fc4fd7a5937d746f7b1988b8
Herbert Gandia <hgandia@gmail.com>
2025-11-18 14:38:21 -0500

    Web App Responsive with Bootstrap Cards

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 const apiKey = process.env.NEWS_API_KEY
-const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`
+const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 
 async function fetchNews(){
     try{
         const response = await fetch(url);
         const data = await response.json();
         const { articles } = data;
-        console.log(data);
         displayNews(articles);
 
     } catch (error) {
@@ -17,12 +16,52 @@ async function fetchNews(){
 fetchNews();
 
 const displayNews = (articles) => {
-    const newsDiv = document.querySelector('#news');
+    
+    //For Responsiveness of the mobile app
+    const containerDiv = document.querySelector('#news');
+    containerDiv.className = 'container-md';
+    const rowDiv = document.createElement('div');
+    rowDiv.className = 'row';
+    containerDiv.appendChild(rowDiv);
+
     for(const article of articles) {
-        const articleDiv = document.createElement('div');
-        const title = document.createElement('h4');
-        title.textContent = article.title;
-        articleDiv.appendChild(title);
-        newsDiv.appendChild(articleDiv);
+
+        const { title, description, url, urlToImage } = article;
+
+        const columnDiv = document.createElement('div');
+        columnDiv.className = 'col-12 col-md-6 col-lg-4 col-xl-3 mb-4';
+        
+        const card = document.createElement('div');
+        card.className = 'card h-100';
+
+        const cardLink = document.createElement('a');
+        cardLink.href = url;
+        cardLink.target = '_blank';
+
+        const cardImg = document.createElement('img');
+        cardImg.src = urlToImage;
+        cardImg.alt = 'newsPic';
+        cardImg.className = 'card-img-top p-2';
+        
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+
+        const cardTitle = document.createElement('h4');
+        cardTitle.className = 'card-title';
+        cardTitle.textContent = title;
+
+        const cardText = document.createElement('p');
+        cardText.className = 'card-text';
+        cardText.textContent = description;
+        
+        cardLink.appendChild(cardImg);
+        card.appendChild(cardLink);
+
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(cardText);
+        card.appendChild(cardBody);
+
+        columnDiv.appendChild(card);
+        rowDiv.appendChild(columnDiv);
     }
 };
